e4482b737b6ac17590e222f2449123dde58b3e90
田村浩一郎 <koichirot11@gmail.com>
2017-06-28 16:28:02 +0900

    minor change

--- translation/utils.py
+++ translation/utils.py
@@ -8,33 +8,33 @@ import google_api
 
 def translate_text_with_model(text, source, target, model="nmt"):
     """
-        :param text: source text
-            :param source: source language
-                :param target: target language
-                    :param model: using model (defualt:nmt)
-                        :return:
-                            """
-                                # set token here
-                                    token = google_api.google_api_key
-                                        url = "https://translation.googleapis.com/language/translate/v2"
+    :param text: source text
+    :param source: source language
+    :param target: target language
+    :param model: using model (defualt:nmt)
+    :return:
+    """
+    # set token here
+	token = google_api.google_api_key
+    url = "https://translation.googleapis.com/language/translate/v2"
 
     payload = {
-            'target': target,
-                    'source': source,
-                            'q': text,
-                                    'model': model,
-                                            'key': token
-                                                }
+	            'target': target,
+	            'source': source,
+	            'q': text,
+	            'model': model,
+	            'key': token
+            	}
 
     headers = {
-            'Content-Type': 'application/json',
+            	'Content-Type': 'application/json',
                     # 'Authorization': 'Bearer ' + token,
-                        }
+            }
 
     response = requests.get(url, params=payload, headers=headers)
 
     # JSON decode
-        jObj = json.loads(response.text)
+    jObj = json.loads(response.text)
 
     # print(jObj)
-        return jObj
+    return jObj
