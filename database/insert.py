f4b67d7f61edee077bc469324f74cdc3e5b895d5
田村浩一郎 <koichirot11@gmail.com>
2017-07-07 15:36:54 +0900

    debug

--- database/insert.py
+++ database/insert.py
@@ -10,7 +10,7 @@ from utils import *
 if __name__ == "__main__":
 	con = mysql.connector.connect(user="root", password="c22h20o13",
 								  host="localhost", database="wordpress", charset='utf8')
-	select_sql = "SELECT `id`, `post_title`, `post_content` from `wp_posts` WHERE (`post_type` == 'topic' OR `post_type` == 'reply') AND `post_partner` IS NULL;"
+	select_sql = "SELECT `id`, `post_title`, `post_content` from `wp_posts` WHERE (`post_type` = 'topic' OR `post_type` = 'reply') AND `post_partner` IS NULL;"
 	df = pd.read_sql(select_sql, con)
 	for index, row in df.iterrows():
 		print(row["id"])
